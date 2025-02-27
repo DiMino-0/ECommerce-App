7a832c0a30ae1b9b4862513e662bf105c4709ce8
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2025-02-27 14:49:18 -0500

    Removing boilerplate from pnpm create

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+const homeMsg = 'Home View'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>{{ homeMsg }}</h1>
   </main>
 </template>
