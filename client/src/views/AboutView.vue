75e5eace7a9f3731a0c8905ec871fbf53cfd0261
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2025-03-06 14:34:53 -0500

    using lib @vueuse

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+import { useNow } from '@vueuse/core'
+
+const { now, pause, resume } = useNow({ controls: true })
+</script>
 <template>
   <div class="about">
     <h1 class="title has-text-black is-1">This is the About page!</h1>
+    <p class="subtitle has-text-black is-3">Current time: {{ now }}</p>
+    <button class="button" v-on:click="pause">Pause</button>
+    <button class="button" v-on:click="resume">Resume</button>
   </div>
 </template>
 
