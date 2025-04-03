6f0c94536df7ef33d09320cb75b8d92cdcf0d2b3
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2025-04-03 14:37:20 -0400

    Custom Error handling

--- server/models/products.js
+++ server/models/products.js
@@ -1,4 +1,7 @@
 const data = require("../data/products.json");
+const { CustomError, statusCodes } = require("./errors");
+
+const isAdmin = true; //placeholder admin check
 
 async function getAll() {
   return data;
@@ -7,12 +10,15 @@ async function getAll() {
 async function get(id) {
   const item = data.items.find((item) => item.id == id);
   if (!item) {
-    throw new Error("Item not found", { status: 404 });
+    throw new CustomError("Item not found", statusCodes.NOT_FOUND);
   }
   return item;
 }
 
 async function create(item) {
+  if (!isAdmin) {
+    throw CustomError("You are not authorized", statusCodes.UNAUTHORIZED);
+  }
   const newItem = {
     id: data.items.length + 1,
     ...item,
